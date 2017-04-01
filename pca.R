a1f8191731b58485bc06abf0d4352d9b84507d92
Amanda Landi <alandi@simons-rock.edu>
2017-04-01 19:33:21 -0400

    Update 'pca.R'

--- pca.R
+++ pca.R
@@ -1,45 +1,56 @@
+# compile the function, pca, first
+# otherwise R does not recognize
 
+#
+# param: data - a matrix
+# param: dim_reduced - the dimension you want to reduce data to
+#
+# returns a list containing the principal components, 
+#                the principal values, the low-rank representation of data, 
+#                and the relative accuracy of the low-rank representation
+#
+pca <- function(data, dim_reduced){
+  
+  # get the dimensions of the data
+  N = dim(data)
+  num_samples =  
+  num_features = 
+  
+  # determine the covariance matrix of data
+  C = 
+  
+  # find the eigen decomposition
+  eigs_decomp = 
+  
+  # determine which columns of the eigenvector matrix are the principal components
+  principal_components = 
+  
+  # X is the transformed data
+  X = 
+  # This is how we get the low-dimensional representation of data
+  repr = t(X) %*% t(principal_components)
+  # how we calculate the relative accuracy
+  error = norm( data - repr, '2')/norm(data, '2')
+      
+  principal_values = 
+  
+  return( list("transform" = principal_components, "values" = principal_values, "representation" = repr, "relative residual" = error))
+  
+}
+
+# you'll need to install a library called imager in order to read in the image
 library(imager)
 
 image = load.image("./virginica.jpg")
 gray = grayscale(image)
 data = as.matrix(gray[,,1,1])
 
-dim_reduced = 20
+#determine your own values for the rank
+dim_reduced = 
 
 results = pca(data, dim_reduced)
 
+# what does your image look like?  Include it in your final write up.
 plot(as.cimg(results$representation))
-print(results$`relative residual`)
 
-pca <- function(data, dim_reduced){
-  
-  N = dim(data); num_samples = N[1]; num_features = N[2];
-  
-  C = cov(data)
-  
-  eigs_decomp = eigen(C, symmetric = TRUE)
-  
-  principal_components = eigs_decomp$vectors[,1:dim_reduced]
-  
-  D = dim(principal_components)
-  
-  if(D[1] != num_samples){
-    
-    X = t(principal_components) %*% t(data)
-    repr = t(X) %*% t(principal_components)
-    error = norm( data - repr, '2')/norm(data, '2')
-      
-  }else{
-  
-    X = t(principal_components) %*% data
-    repr = principal_components %*% X
-    error = norm(data - repr, '2')/norm(data, '2')
-  
-  }
-  
-  principal_values = eigs_decomp$values[1:dim_reduced]
-  
-  return( list("transform" = principal_components, "values" = principal_values, "representation" = repr, "relative residual" = error))
-  
-}
+
