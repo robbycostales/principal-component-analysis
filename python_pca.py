36a84a872024b6edb5585a498235053a3f72861e
Robby Costales <rcostales15@simons-rock.edu>
2017-04-12 21:29:01 -0400

    bloopity blah blah

--- python_pca.py
+++ python_pca.py
@@ -1,40 +1,100 @@
+# Author: Robert Costales
+# Date: 2017 04 12
+# Language: Python 3
+# Overview: This program converts the matrix of pixel values of an image
+#           into a lower dimensional space using the PCA method
+
+# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ IMPORT STATEMENTS _-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+
+import copy
+from scipy import misc
 import numpy as np
-from numpy import linalg as la
-import pandas as pd
+import matplotlib.pyplot as plt
+import time
 
-#
-# param: data - the data we want to transform, it's a pandas data frame
-# param: dim_reduced - the size of the transformed data
-#
-# returns a dictionary containing the principal components, principal values, the low-dimensional representation, and relative accuracy
-#
+# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ RUN PREFERENCES _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+
+show_init_image = False     # shows initial image from gray-scale matrix
+
+# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ FUNCTIONS _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+
+
 def pca(data, dim_reduced):
-  
-  	# calculate the dimensions of the data
-    num_samples, num_features = 
-    #find the covariance matrix
-    C = 
-    # find the eigen vectors and eigen values of the covariance matrix
-    eig_val, eig_vec = 
-    # determine which columns in eig_vec are the principal components
-    principal_components =
-    
-    # multiply the principal components and the data to transform    
-    X = 
-    # the low-dimensional representation
-    repr = np.matmul(X.transpose(), principal_components.transpose())
-    # relative accuracy
-    error = np.linalg.norm( data - repr.transpose(), ord = 2)/np.linalg.norm(data, ord = 2)
-     
-    # what are the principal values?  
-    principal_values = 
-    
-    return {"transform":principal_components, "values":principal_values, "representation":repr, "relative residual":error}
-    
-# For the term-document matrix
-data = pd.read_csv(open('./Documents/assoc_pressMat.csv'))
-# choose your own low-rank
-dim_reduced = 
+    """
+    :param data: the data we want to transform (grayscale valued matrix)
+    :param dim_reduced: the size of the transformed data
+    :return: reduced dimension image matrix
+    """
+    # find the covariance matrix
+    y = data
+    yt = np.transpose(y)
+    c = np.cov(y)             # create covariance matrix
+    eig_val, eig_vec = np.linalg.eig(c)                  # eig_val, eig_vec
+    idx = eig_val.argsort()[::-1]
+    eig_val = eig_val[idx]
+    eig_vec = eig_vec[:, idx]
 
-results = pca(data, dim_reduced)
- 
+    q = eig_vec                             # is full eigen-vector matrix
+    qt = np.transpose(q)                    # transpose of q
+    p = qt[0:dim_reduced]                  # takes first dim_reduced rows of qt
+
+    print("P Dimensions:  ", len(p), end=" ")
+    if len(p) != 0:
+        print(len(p[0]))
+    else:
+        print(" ")
+
+
+    v = np.matmul(p, y)
+    vt = np.transpose(v)
+    redyt = np.matmul(vt, p)
+    redy = np.transpose(redyt)
+
+    print("Image Dimensions:  ", len(redy), len(redy[0]))
+    print(" ")
+
+    plt.gray()  # comment this out for fun colors
+    plt.imshow(redy)
+    plt.show()
+
+
+# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ IMAGE PROCESSING _-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+
+rgb = misc.imread('virginica.jpg')  # 441x600x3 array (RGB)
+
+gray = []
+for i in rgb:           # matrix converted to gray-scale values
+    a = []
+    for j in i:
+        a.append(sum(j)/len(j)/255)
+    gray.append(copy.deepcopy(a))
+
+# gray is a 441x600 matrix of gray-scale intensity values between 0 and 1
+
+plt.gray()              # comment this out for fun colors
+plt.imshow(gray)
+if show_init_image:
+    plt.show()
+
+# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ PCA CALL _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+
+print("k = {}".format(441))
+pca(gray, 441)
+print("k = {}".format(310))
+pca(gray, 310)
+print("k = {}".format(231))
+pca(gray, 231)
+print("k = {}".format(100))
+pca(gray, 100)
+print("k = {}".format(50))
+pca(gray, 50)
+print("k = {}".format(25))
+pca(gray, 25)
+print("k = {}".format(7))
+pca(gray, 7)
+print("k = {}".format(3))
+pca(gray, 3)
+print("k = {}".format(1))
+pca(gray, 1)
+print("k = {}".format(0))
+pca(gray, 0)
