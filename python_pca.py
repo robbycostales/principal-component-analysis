182593aef1e9f02f16467a3033371c1e1156b37b
Robby Costales <rcostales15@src-code.simons-rock.edu>
2017-04-19 15:10:30 -0400

    this push is pretty late

--- python_pca.py
+++ python_pca.py
@@ -26,13 +26,14 @@ show_init_image = False     # shows initial image from gray-scale matrix
 # _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ FUNCTIONS _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
 
 
-def pca(data, dim_reduced, show_plot=True):
+def pca(data, dim_reduced, show_plot=True, labels=[]):
     """
     :param data: the data we want to transform (grayscale valued matrix)
     :param dim_reduced: the size of the transformed data
     :return: error, (n, m, c)
     """
     # find the covariance matrix
+    print(labels)
     y = data
     yt = np.transpose(y)
     c = np.cov(y)             # create covariance matrix
@@ -41,10 +42,12 @@ def pca(data, dim_reduced, show_plot=True):
     eig_val = eig_val[idx]
     eig_vec = eig_vec[:, idx]
 
+    labels = labels[idx]
+
     q = eig_vec                             # is full eigen-vector matrix
     qt = np.transpose(q)                    # transpose of q
-    p = qt[0:dim_reduced]                  # takes first dim_reduced rows of qt
-
+    p = qt[0:dim_reduced]                 # takes first dim_reduced rows of qt
+    labels = labels[0:dim_reduced]
 
     v = np.matmul(p, y)
     n = len(qt)
@@ -71,6 +74,7 @@ def pca(data, dim_reduced, show_plot=True):
         print("Image Dimensions:  ", len(redy), len(redy[0]))
         print(" ")
 
+    print(labels)
     if show_plot:
         plt.clf()
         plt.gray()  # comment this out for fun colors
@@ -80,80 +84,80 @@ def pca(data, dim_reduced, show_plot=True):
         plt.gcf()
         plt.show()
 
-    return error, (n, m, c)
+    return error, (n, m, c), v
 
 # _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ IMAGE PROCESSING _-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+if __name__ == '__main__':
+    rgb = misc.imread('virginica.jpg')  # 441x600x3 array (RGB)
 
-rgb = misc.imread('virginica.jpg')  # 441x600x3 array (RGB)
+    gray = []
+    for i in rgb:           # matrix converted to gray-scale values
+        a = []
+        for j in i:
+            a.append(sum(j)/len(j)/255)
+        gray.append(copy.deepcopy(a))
 
-gray = []
-for i in rgb:           # matrix converted to gray-scale values
-    a = []
-    for j in i:
-        a.append(sum(j)/len(j)/255)
-    gray.append(copy.deepcopy(a))
+    # gray is a 441x600 matrix of gray-scale intensity values between 0 and 1
 
-# gray is a 441x600 matrix of gray-scale intensity values between 0 and 1
+    plt.gray()              # comment this out for fun colors
+    plt.imshow(gray)
+    if show_init_image:
+        plt.show()
 
-plt.gray()              # comment this out for fun colors
-plt.imshow(gray)
-if show_init_image:
+    # _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ PCA CALL _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+
+    #
+    # When we decrease our k value from the original dimension of the nxn covariance matrix,
+    # and we multiply vT with P (for that k), we get a compressed image
+    #
+
+    show_int_images = True
+    print("k = {}".format(441))
+    pca(gray, 441, show_int_images)
+    print("k = {}".format(310))
+    pca(gray, 310, show_int_images)
+    print("k = {}".format(231))
+    pca(gray, 231, show_int_images)
+    print("k = {}".format(100))
+    pca(gray, 100, show_int_images)
+    print("k = {}".format(50))
+    pca(gray, 50, show_int_images)
+    print("k = {}".format(25))
+    pca(gray, 25, show_int_images)
+    print("k = {}".format(7))
+    pca(gray, 7, show_int_images)
+    print("k = {}".format(3))
+    pca(gray, 3, show_int_images)
+    print("k = {}".format(1))
+    pca(gray, 1, show_int_images)
+    print("k = {}".format(0))
+    pca(gray, 0, show_int_images)
+
+    # _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ ERROR _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
+
+    min_aic = -1
+    min_k = -1
+    aics = []
+    ks = range(1, 200, 3)
+
+    print("Please wait for results... (may take up to a minute--trying to fix)\n")
+    for i in ks:
+        error, (n, m, c) = pca(gray, i, False)
+        aic = ((n+m)*i)/c + error*255*8
+        aics.append(aic)
+        if aic < min_aic or min_aic == -1:
+            min_k = i
+            min_aic = aic
+
+
+    print("Minimized error around k={0}".format(min_k))
+
+    plt.clf()
+    plt.scatter(ks, aics)
+    plt.title("AIC vs K")
+    fig = plt.gcf()
+    fig.canvas.set_window_title("Optimizing K")
     plt.show()
 
-# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ PCA CALL _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
-
-#
-# When we decrease our k value from the original dimension of the nxn covariance matrix,
-# and we multiply vT with P (for that k), we get a compressed image
-#
-
-show_int_images = False
-print("k = {}".format(441))
-pca(gray, 441, show_int_images)
-print("k = {}".format(310))
-pca(gray, 310, show_int_images)
-print("k = {}".format(231))
-pca(gray, 231, show_int_images)
-print("k = {}".format(100))
-pca(gray, 100, show_int_images)
-print("k = {}".format(50))
-pca(gray, 50, show_int_images)
-print("k = {}".format(25))
-pca(gray, 25, show_int_images)
-print("k = {}".format(7))
-pca(gray, 7, show_int_images)
-print("k = {}".format(3))
-pca(gray, 3, show_int_images)
-print("k = {}".format(1))
-pca(gray, 1, show_int_images)
-print("k = {}".format(0))
-pca(gray, 0, show_int_images)
-
-# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_ ERROR _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ #
-
-min_aic = -1
-min_k = -1
-aics = []
-ks = range(1, 200, 3)
-
-print("Please wait for results... (may take up to a minute--trying to fix)\n")
-for i in ks:
-    error, (n, m, c) = pca(gray, i, False)
-    aic = ((n+m)*i)/c + error*255*8
-    aics.append(aic)
-    if aic < min_aic or min_aic == -1:
-        min_k = i
-        min_aic = aic
-
-
-print("Minimized error around k={0}".format(min_k))
-
-plt.clf()
-plt.scatter(ks, aics)
-plt.title("AIC vs K")
-fig = plt.gcf()
-fig.canvas.set_window_title("Optimizing K")
-plt.show()
-
-print("k = {}\n".format(min_k))
-pca(gray, min_k)
+    print("k = {}\n".format(min_k))
+    pca(gray, min_k)
---
+++ words.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import python_pca as pca
+import numpy as np
+# df = pd.read_csv("./assoc_press/assoc_pressMat.csv")
+# df.as_matrix()
+
+df=pd.read_csv("./assoc_press/assoc_pressMat.csv", sep=',',header=None)
+#print(df)
+
+y_pre = df.values
+
+y = [j[1:] for j in y_pre[1:]] # data
+
+for i in range(len(y)):
+    for j in range(len(y[i])):
+        y[i][j] = int(y[i][j])
+
+data = []
+for i in range(len(y)):
+    row = []
+    for j in range(len(y[i])):
+        row.append(y[i][j])
+    data.append(row)
+
+labels = y_pre[0][1:]
+
+
+print(data)
+
+pca.pca(data, 1, True, labels)
